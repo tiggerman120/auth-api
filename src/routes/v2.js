f50af42ac8c27873708d3ac0be3ab4e89d495f91
Garrett Cintron <grrttcintron@gmail.com>
2020-12-16 21:21:34 -0800

    task 2 complete. jest working

---
+++ src/routes/v2.js
@@ -0,0 +1,101 @@
+'use strict';
+
+const fs = require('fs');
+const express = require('express');
+const bearerAuth = require('../middleware/bearer.js');
+const router = express.Router();
+const models = new Map();
+const Collection = require('../models/data-collection.js');
+const User = require('../models/users.js');
+const basicAuth = require('../middleware/basic.js');
+const permissions = require('../middleware/acl.js');
+
+router.post('/signup', async (req, res, next) => {
+  try {
+    let user = new User(req.body);
+    const userRecord = await user.save();
+    const output = {
+      user: userRecord,
+      token: userRecord.token,
+    };
+    res.status(201).json(output);
+  } catch (e) {
+    next(e.message);
+  }
+});
+
+router.post('/signin', basicAuth, (req, res, next) => {
+  const user = {
+    user: req.user,
+    token: req.user.token,
+  };
+  res.status(200).json(user);
+});
+
+router.get('/users', bearerAuth, permissions('delete'), async (req, res, next) => {
+  const users = await User.find({});
+  const list = users.map(user => user.username);
+  res.status(200).json(list);
+});
+
+router.get('/secret', bearerAuth, async (req, res, next) => {
+  res.status(200).send('Welcome to the secret area');
+});
+
+router.param('model', (req, res, next) => {
+  const modelName = req.params.model;
+  if (models.has(modelName)) {
+    req.model = models.get(modelName);
+    next();
+  } else {
+    const fileName = `${__dirname}/../models/${modelName}/model.js`;
+    if (fs.existsSync(fileName)) {
+      const model = require(fileName);
+      models.set(modelName, new Collection(model));
+      req.model = models.get(modelName);
+      next();
+    }
+    else {
+      next('Invalid Model');
+    }
+  }
+});
+
+router.get('/:model', bearerAuth, handleGetAll);
+router.get('/:model/:id', bearerAuth, handleGetOne);
+router.post('/:model', bearerAuth, permissions('create'), handleCreate);
+router.put('/:model/:id', bearerAuth, permissions('update'), handleUpdate);
+router.delete('/:model/:id', bearerAuth, permissions('delete'), handleDelete);
+
+async function handleGetAll(req, res) {
+  let allRecords = await req.model.get();
+  res.status(200).json(allRecords);
+}
+
+async function handleGetOne(req, res) {
+  const id = req.params.id;
+  let theRecord = await req.model.get(id);
+  res.status(200).json(theRecord);
+}
+
+async function handleCreate(req, res) {
+  let obj = req.body;
+  let newRecord = await req.model.create(obj);
+  res.status(201).json(newRecord);
+}
+
+async function handleUpdate(req, res) {
+  const id = req.params.id;
+  const obj = req.body;
+  let updatedRecord = await req.model.update(id, obj);
+  res.status(200).json(updatedRecord);
+}
+
+async function handleDelete(req, res) {
+  let id = req.params.id;
+  let deletedRecord = await req.model.delete(id);
+  res.status(200).json(deletedRecord);
+}
+
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const errorHandler = require('./error-handlers/500.js');
 const logger = require('./middleware/logger.js');
 
 const v1Routes = require('./routes/v1.js');
-
+const v2Routes = require('./routes/v2.js');
 const app = express();
 
 // App Level MW
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(logger);
 
 app.use('/api/v1', v1Routes);
-app.use(v1Routes);
+app.use('/api/v2', v2Routes);
 
 app.use('*', notFoundHandler);
 app.use(errorHandler);
